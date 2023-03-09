c3f656e5f28200b9e6fd76592c6647bc58afcb8e
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2023-03-09 11:37:19 +0100

    Update App.js

--- GUI/project/src/App.js
+++ GUI/project/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import "./App.css";
-//import "leflet/dist/leaflet.css";
 
-//importin Leaflet Components
+import "leaflet/dist/leaflet.css";
+
+//importing Leaflet Components
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet' 
 
 
@@ -11,7 +12,7 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 function App() {
   return (
     <>
-      <MapContainer center={[47.5349, 7.6416]} zoom={15} scrollWheelZoom={true}>
+      <MapContainer center={[51.505, -0.09]} zoom={15} scrollWheelZoom={true}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution='&copy; OpenStreetMap contributors'/>
       </MapContainer>
