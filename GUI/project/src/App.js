b2aa7c2d4389d82e4e489aaebd71551c69d6cabf
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2023-03-09 15:53:28 +0100

    update GUI

--- GUI/project/src/App.js
+++ GUI/project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./App.css";
 
 import { MapContainer, TileLayer, FeatureGroup, Polygon} from 'react-leaflet';
@@ -7,51 +7,102 @@ import { useRef } from 'react';
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 
-import L, {Point, DivIcon, polygon, map} from 'leaflet';
+
+import L, {Point, DivIcon, polygon, map, circle} from 'leaflet';
 
 
-import { Button,Toolbar } from '@mui/material';
-
-
- 
-function App() {
+import { Button,AppBar,Toolbar} from '@mui/material';
 
 
 
+const App = () => {
+  const [center, setCenter] = useState({ lat: 24.4539, lng: 54.3773 });
+  const [mapLayers, setMapLayers] = useState([]);
 
-var osm = (<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-/>)
+  const ZOOM_LEVEL = 12;
+  const mapRef = useRef();
 
-const polygon = [
-                [46.8011, 8.2266],
-                [46.8012, 8.2266],
-                [46.8011, 8.2267],
-                [46.8011, 8.2266],
-              ]
+  const _onCreate = (e) => {
+    console.log(e);
 
+    const { layerType, layer } = e;
+    if (layerType === "polygon") {
+      const { _leaflet_id } = layer;
 
+      setMapLayers((layers) => [
+        ...layers,
+        { id: _leaflet_id, latlngs: layer.getLatLngs()[0] },
+      ]);
+    }
+  };
 
-return (
-  <>
-  <Toolbar variant='regular'>
-  Timber Volume Calculation
-  </Toolbar>
+  const _onEdited = (e) => {
+    console.log(e);
+    const {
+      layers: { _layers },
+    } = e;
 
-  <MapContainer center={[46.801111, 8.226667]} zoom={13} scrollWheelZoom={true}>
-  {osm}
-  <Polygon
-    positions={polygon}
-  />
-  </MapContainer>
+    Object.values(_layers).map(({ _leaflet_id, editing }) => {
+      setMapLayers((layers) =>
+        layers.map((l) =>
+          l.id === _leaflet_id
+            ? { ...l, latlngs: { ...editing.latlngs[0] } }
+            : l
+        )
+      );
+    });
+  };
 
-  <Button variant="outlined">Test</Button>
-  </>
+  const _onDeleted = (e) => {
+    console.log(e);
+    const {
+      layers: { _layers },
+    } = e;
 
+    Object.values(_layers).map(({ _leaflet_id }) => {
+      setMapLayers((layers) => layers.filter((l) => l.id !== _leaflet_id));
+    });
+  };
 
+  return (
+    <>
+
+      <div className="row">
+        <div className="col text-center">
+          
+            <AppBar position='sticky' color='primary'>
+              <Toolbar variant=' '>Timber Volume Calculation</Toolbar>
+            </AppBar>
+          <div className="col">
+
+            <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+              <FeatureGroup>
+                <EditControl
+                  position="topright"
+                  onCreated={_onCreate}
+                  onEdited={_onEdited}
+                  onDeleted={_onDeleted}
+                  draw={{
+                    rectangle: false,
+                    polyline: false,
+                    circle: false,
+                    circlemarker: false,
+                    marker: false,
+                  }}
+                />
+              </FeatureGroup>
+
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+            </MapContainer>
+
+            <pre className="text-left">{JSON.stringify(mapLayers, 0, 2)}</pre>
+          </div>
+        </div>
+      </div>
+    </>
   );
-
-
-}
+};
 
 export default App;
--- GUI/project/src/index.js
+++ GUI/project/src/index.js
@@ -8,5 +8,6 @@ import '@fontsource/roboto/700.css';
 
 import App from './App';
 
+
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(<App />);
