d57cbbe0aacb9bd762a93f442b4a24921f727f7a
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2023-03-09 11:43:40 +0100

    Update App.js

--- GUI/project/src/App.js
+++ GUI/project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import "./App.css";
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { useMap } from 'react-leaflet/hooks';
+
 
 import "leaflet/dist/leaflet.css";
+import "./App.css";
 
-//importing Leaflet Components
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet' 
 
 
 
