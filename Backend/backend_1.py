75c58340ac2922a1f3bb1a8c7c7751dbe70b9e83
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2023-03-10 14:15:55 +0100

    Backend angepasst

---
+++ Backend/backend_1.py
@@ -0,0 +1,33 @@
+# Imports für Rest API Schnittstelle
+import uvicorn
+from fastapi import FastAPI,Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Rest API
+@app.post("/polyjson")
+async def polyjson(info :Request):
+    req_info = await info.json()
+    new_polygon = req_info['input']
+    print(f"this is my data:{new_polygon}")
+    
+@app.get("/result")
+async def root() :
+    return {"Anzahl":5,
+            "Volumen":21020
+            }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
